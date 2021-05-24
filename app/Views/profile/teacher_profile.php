9b78e3b11d3331b01b8537f7dce2c7d5fe0f149a
zahlulapaaja <zahlul.2001@gmail.com>
2021-05-24 21:50:31 +0700

    pop up modal hasil report

--- app/Views/profile/teacher_profile.php
+++ app/Views/profile/teacher_profile.php
@@ -159,180 +159,203 @@
             </div>
         </div>
     </div>
+</div>
 
-    <!--Modal Laporkan Pengguna-->
-    <div class="modal opacity-0 pointer-events-none fixed w-full h-full top-0 left-0 flex items-center justify-center overflow-y-auto" id="reportModal">
-        <div class="modal-overlay absolute w-full h-full bg-gray-900 opacity-75 bg-scroll"></div>
+<!--Modal Laporkan Pengguna-->
+<div class="modal opacity-0 pointer-events-none fixed w-full h-full top-0 left-0 flex items-center justify-center overflow-y-auto" id="reportModal">
+    <div class="modal-overlay absolute w-full h-full bg-gray-900 opacity-75 bg-scroll"></div>
 
-        <div class="modal-container bg-pewter w-full md:max-w-2xl mx-auto md:mt-20 rounded shadow-lg z-50">
+    <div class="modal-container bg-pewter w-full md:max-w-2xl mx-auto md:mt-20 rounded shadow-lg z-50">
 
-            <!-- Add margin if you want to see some of the overlay behind the modal-->
-            <div class="modal-content py-4 text-center px-6">
-                <!--Title-->
-                <div class="relative flex flex-col items-center pb-8">
-                    <p class="text-2xl text-darkBlue font-bold font-heading">Laporkan Pengguna</p>
-                </div>
+        <!-- Add margin if you want to see some of the overlay behind the modal-->
+        <div class="modal-content py-4 text-center px-6">
+            <!--Title-->
+            <div class="relative flex flex-col items-center pb-8">
+                <p class="text-2xl text-darkBlue font-bold font-heading">Laporkan Pengguna</p>
+            </div>
 
-                <!--Body-->
-                <form action="#" class="">
-                    <table class="table-auto w-full text-sm -mx-10">
-                        <tbody>
-                            <tr>
-                                <td class="text-darkBlue font-semibold text-right align-top" rowspan="6">
-                                    <label for="alasan">Menurut saya:</label>
-                                </td>
-                                <td class="pl-5 py-0.5 text-left max-w-xs">
-                                    <label class="inline-flex items-center">
-                                        <input type="radio" class="form-radio h-4 w-4 text-gray-600" name="alasan" value="alasan1">
-                                        <span class="ml-2">Orang ini membuat saya kesal</span>
-                                    </label>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="pl-5 py-0.5 text-left max-w-xs">
-                                    <label class="inline-flex items-center">
-                                        <input type="radio" class="form-radio h-4 w-4 text-gray-600" name="alasan" value="alasan2">
-                                        <span class="ml-2">Orang ini melakukan spam</span>
-                                    </label>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="pl-5 py-0.5 text-left max-w-xs">
-                                    <label class="inline-flex items-center">
-                                        <input type="radio" class="form-radio h-4 w-4 text-gray-600" name="alasan" value="alasan3">
-                                        <span class="ml-2">Orang ini tidak menyelesaikan transaksi yang disepakati</span>
-                                    </label>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="pl-5 py-0.5 text-left max-w-xs">
-                                    <label class="inline-flex items-center">
-                                        <input type="radio" class="form-radio h-4 w-4 text-gray-600" name="alasan" value="alasan4">
-                                        <span class="ml-2">Orang ini mengirim sesuatu yang tidak pantas atau menyinggung</span>
-                                    </label>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="pl-5 py-0.5 text-left max-w-xs">
-                                    <label class="inline-flex items-center">
-                                        <input type="radio" class="form-radio h-4 w-4 text-gray-600" name="alasan" value="alasan5">
-                                        <span class="ml-2">Ini akun palsu</span>
-                                    </label>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="pl-5 py-0.5 text-left max-w-xs">
-                                    <label class="inline-flex items-center">
-                                        <input type="radio" class="form-radio h-4 w-4 text-gray-600" name="alasan" value="alasan6">
-                                        <span class="ml-2">Lainnya</span>
-                                    </label>
-                                </td>
-                            </tr>
-                            <tr class="">
-                                <td class="flex items-start justify-end text-darkBlue font-semibold py-4">
-                                    <label for="deskripsi-diri">Detail (opsional):</label>
-                                </td>
-                                <td class="pl-5 py-4">
-                                    <textarea id="deskripsi-diri" name="deskripsi_diri" rows="4" class="w-full bg-white text-gray-800 border-2 border-opacity-50 border-gray-600 max-h-28 px-2">
+            <!--Body-->
+            <form action="#" class="">
+                <table class="table-auto w-full text-sm -mx-10">
+                    <tbody>
+                        <tr>
+                            <td class="text-darkBlue font-semibold text-right align-top" rowspan="6">
+                                <label for="alasan">Menurut saya:</label>
+                            </td>
+                            <td class="pl-5 py-0.5 text-left max-w-xs">
+                                <label class="inline-flex items-center">
+                                    <input type="radio" class="form-radio h-4 w-4 text-gray-600" name="alasan" value="alasan1">
+                                    <span class="ml-2">Orang ini membuat saya kesal</span>
+                                </label>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="pl-5 py-0.5 text-left max-w-xs">
+                                <label class="inline-flex items-center">
+                                    <input type="radio" class="form-radio h-4 w-4 text-gray-600" name="alasan" value="alasan2">
+                                    <span class="ml-2">Orang ini melakukan spam</span>
+                                </label>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="pl-5 py-0.5 text-left max-w-xs">
+                                <label class="inline-flex items-center">
+                                    <input type="radio" class="form-radio h-4 w-4 text-gray-600" name="alasan" value="alasan3">
+                                    <span class="ml-2">Orang ini tidak menyelesaikan transaksi yang disepakati</span>
+                                </label>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="pl-5 py-0.5 text-left max-w-xs">
+                                <label class="inline-flex items-center">
+                                    <input type="radio" class="form-radio h-4 w-4 text-gray-600" name="alasan" value="alasan4">
+                                    <span class="ml-2">Orang ini mengirim sesuatu yang tidak pantas atau menyinggung</span>
+                                </label>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="pl-5 py-0.5 text-left max-w-xs">
+                                <label class="inline-flex items-center">
+                                    <input type="radio" class="form-radio h-4 w-4 text-gray-600" name="alasan" value="alasan5">
+                                    <span class="ml-2">Ini akun palsu</span>
+                                </label>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="pl-5 py-0.5 text-left max-w-xs">
+                                <label class="inline-flex items-center">
+                                    <input type="radio" class="form-radio h-4 w-4 text-gray-600" name="alasan" value="alasan6">
+                                    <span class="ml-2">Lainnya</span>
+                                </label>
+                            </td>
+                        </tr>
+                        <tr class="">
+                            <td class="flex items-start justify-end text-darkBlue font-semibold py-4">
+                                <label for="deskripsi-diri">Detail (opsional):</label>
+                            </td>
+                            <td class="pl-5 py-4">
+                                <textarea id="deskripsi-diri" name="deskripsi_diri" rows="4" class="w-full bg-white text-gray-800 border-2 border-opacity-50 border-gray-600 max-h-28 px-2">
                                     </textarea>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    <!--Footer-->
-                    <div class="flex flex-row justify-center pt-2 text-sm">
-                        <button type="submit" class="w-auto cursor-pointer px-8 mx-4 text-center py-0.5 rounded-xl bg-darkBlue text-white border-2 border-darkBlue hover:bg-white hover:text-darkBlue focus:outline-none my-4">
-                            Kirim
-                        </button>
-                        <span class="modal-close w-auto cursor-pointer px-8 mx-4 text-center py-0.5 rounded-xl bg-blueGray text-white border-2 border-darkBlue hover:bg-white hover:text-darkBlue focus:outline-none my-4">
-                            Batal
-                        </span>
-                    </div>
-                </form>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <!--Footer-->
+                <div class="flex flex-row justify-center pt-2 text-sm">
+                    <button type="submit" class="w-auto cursor-pointer px-8 mx-4 text-center py-0.5 rounded-xl bg-darkBlue text-white border-2 border-darkBlue hover:bg-white hover:text-darkBlue focus:outline-none my-4">
+                        Kirim
+                    </button>
+                    <span class="modal-close w-auto cursor-pointer px-8 mx-4 text-center py-0.5 rounded-xl bg-blueGray text-white border-2 border-darkBlue hover:bg-white hover:text-darkBlue focus:outline-none my-4">
+                        Batal
+                    </span>
+                </div>
+            </form>
 
+        </div>
+    </div>
+</div>
+
+<!-- Modal Belum Login -->
+<div class="modal opacity-0 pointer-events-none fixed w-full h-full z-50 top-0 left-0 flex items-center justify-center overflow-y-auto" id="reportDoneModal">
+    <div class="modal-overlay modal-close absolute w-full h-full bg-gray-900 opacity-75 bg-scroll"></div>
+
+    <div class="modal-container bg-pewter w-full md:max-w-xs mx-auto md:mt-20 rounded-xl shadow-lg z-50">
+
+        <!-- Add margin if you want to see some of the overlay behind the modal-->
+        <div class="modal-content pt-4 text-center">
+            <!--Title-->
+            <div class="relative flex flex-col items-center py-2">
+                <p class="text-base text-darkBlue font-body px-5">Terima kasih, kami akan meninjau laporan yang Anda kirimkan</p>
+            </div>
+            <div class="flex flex-row justify-center text-sm">
+                <button class="modal-close w-auto cursor-pointer px-8 mx-4 text-center py-0.5 rounded-xl bg-darkBlue text-white border-2 border-darkBlue hover:bg-white hover:text-darkBlue focus:outline-none my-4">
+                    Kembali
+                </button>
             </div>
         </div>
     </div>
-
-
-
-    <script>
-        /*Toggle dropdown list*/
-        function toggleDD(myDropMenu) {
-            document.getElementById(myDropMenu).classList.toggle("invisible");
-        }
-        /*Filter dropdown options*/
-        function filterDD(myDropMenu, myDropMenuSearch) {
-            var input, filter, ul, li, a, i;
-            input = document.getElementById(myDropMenuSearch);
-            filter = input.value.toUpperCase();
-            div = document.getElementById(myDropMenu);
-            a = div.getElementsByTagName("a");
-            for (i = 0; i < a.length; i++) {
-                if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
-                    a[i].style.display = "";
-                } else {
-                    a[i].style.display = "none";
-                }
-            }
-        }
-        // Close the dropdown menu if the user clicks outside of it
-        window.onclick = function(event) {
-            if (!event.target.matches('.drop-button') && !event.target.matches('.drop-search')) {
-                var dropdowns = document.getElementsByClassName("dropdownlist");
-                for (var i = 0; i < dropdowns.length; i++) {
-                    var openDropdown = dropdowns[i];
-                    if (!openDropdown.classList.contains('invisible')) {
-                        openDropdown.classList.add('invisible');
-                    }
-                }
-            }
-        }
+</div>
 
 
 
 
-        var openmodal = document.querySelectorAll('.modal-open')
-        let selectedModalTargetId = ''
-        for (var i = 0; i < openmodal.length; i++) {
-            openmodal[i].addEventListener('click', function(event) {
-                selectedModalTargetId = event.target.attributes.getNamedItem('data-target').value
-                event.preventDefault()
-                toggleModal()
-            })
-        }
-
-        const overlay = document.querySelector('.modal-overlay')
-        overlay.addEventListener('click', toggleModal)
-
-        var closemodal = document.querySelectorAll('.modal-close')
-        for (var i = 0; i < closemodal.length; i++) {
-            closemodal[i].addEventListener('click', toggleModal)
-        }
-
-        document.onkeydown = function(evt) {
-            evt = evt || window.event
-            var isEscape = false
-            if ("key" in evt) {
-                isEscape = (evt.key === "Escape" || evt.key === "Esc")
+<script>
+    /*Toggle dropdown list*/
+    function toggleDD(myDropMenu) {
+        document.getElementById(myDropMenu).classList.toggle("invisible");
+    }
+    /*Filter dropdown options*/
+    function filterDD(myDropMenu, myDropMenuSearch) {
+        var input, filter, ul, li, a, i;
+        input = document.getElementById(myDropMenuSearch);
+        filter = input.value.toUpperCase();
+        div = document.getElementById(myDropMenu);
+        a = div.getElementsByTagName("a");
+        for (i = 0; i < a.length; i++) {
+            if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+                a[i].style.display = "";
             } else {
-                isEscape = (evt.keyCode === 27)
-            }
-            if (isEscape && document.body.classList.contains('modal-active')) {
-                toggleModal()
+                a[i].style.display = "none";
             }
         }
-
-        function toggleModal() {
-            if (!selectedModalTargetId) {
-                return
+    }
+    // Close the dropdown menu if the user clicks outside of it
+    window.onclick = function(event) {
+        if (!event.target.matches('.drop-button') && !event.target.matches('.drop-search')) {
+            var dropdowns = document.getElementsByClassName("dropdownlist");
+            for (var i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (!openDropdown.classList.contains('invisible')) {
+                    openDropdown.classList.add('invisible');
+                }
             }
-            const body = document.querySelector('body')
-            const modal = document.getElementById(selectedModalTargetId)
-            modal.classList.toggle('opacity-0')
-            modal.classList.toggle('pointer-events-none')
-            body.classList.toggle('modal-active')
         }
-    </script>
+    }
 
-    <?= $this->endSection() ?>
+
+
+
+    var openmodal = document.querySelectorAll('.modal-open')
+    let selectedModalTargetId = ''
+    for (var i = 0; i < openmodal.length; i++) {
+        openmodal[i].addEventListener('click', function(event) {
+            selectedModalTargetId = event.target.attributes.getNamedItem('data-target').value
+            event.preventDefault()
+            toggleModal()
+        })
+    }
+
+    const overlay = document.querySelector('.modal-overlay')
+    overlay.addEventListener('click', toggleModal)
+
+    var closemodal = document.querySelectorAll('.modal-close')
+    for (var i = 0; i < closemodal.length; i++) {
+        closemodal[i].addEventListener('click', toggleModal)
+    }
+
+    document.onkeydown = function(evt) {
+        evt = evt || window.event
+        var isEscape = false
+        if ("key" in evt) {
+            isEscape = (evt.key === "Escape" || evt.key === "Esc")
+        } else {
+            isEscape = (evt.keyCode === 27)
+        }
+        if (isEscape && document.body.classList.contains('modal-active')) {
+            toggleModal()
+        }
+    }
+
+    function toggleModal() {
+        if (!selectedModalTargetId) {
+            return
+        }
+        const body = document.querySelector('body')
+        const modal = document.getElementById(selectedModalTargetId)
+        modal.classList.toggle('opacity-0')
+        modal.classList.toggle('pointer-events-none')
+        body.classList.toggle('modal-active')
+    }
+</script>
+
+<?= $this->endSection() ?>
